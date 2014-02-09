050b2942d9a982d22ad661b820500b3d7716c8dc
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2014-02-09 23:04:18 +0200

    Baconized build

--- build.js
+++ build.js
@@ -1,71 +1,87 @@
 var fs         = require('fs'),
     path       = require('path'),
     _          = require('lodash'),
+    Bacon      = require('baconjs'),
     browserify = require('browserify'),
-    specsOpts  = {
-        basedir: './specs',
-        noParse: [
-            './specs/vendor/mocha.js'
-        ],
-        debug: true
-    },
     createSpecRequireFile = function(to, requiredFiles) {
         var content = requiredFiles.map(function(file) {
-            return 'require(\''+ file +'\');';
+            return 'require(\''+ file.split('.js')[0] +'\');';
         }).join('');
         fs.writeFile(to, content, function(err) {
             if(err) console.log(err);
         });
         return to;
     },
-    specsWalkDone = {
-        err: function err(err) {
-            console.error(err);
-        },
-        success: function done(files) {
-            var allSpecsFileName = createSpecRequireFile('./specs/all.specs.js', files),
-                specs = browserify(specsOpts);
-            specs
-                .add('./main.js')
-                .bundle()
-                .pipe(fs.createWriteStream('./specs/built.js'));
-        }
-    },
     walk = function(opts) {
         var dir = opts.dir,
-            done = opts.done,
             basedir = opts.basedir || opts.dir,
-            filter = opts.filter || function() { return true; },
-            results = [];
-        fs.readdir(dir, function(err, list) {
-            if (err) return done.err(err);
-            var filteredList = list.filter(filter),
-                pending = filteredList.length;
-            if (!pending) return done.success(results);
-            filteredList.forEach(function(file) {
-                file = basedir + path.sep + file;
-                fs.stat(file, function(err, stat) {
-                    if (stat && stat.isDirectory()) {
-                        walk({
-                            dir: file,
-                            basedir: basedir,
-                            done: {
-                                success: function(res) {
-                                    results = results.concat(res);
-                                    if (!--pending) done.success(results);
-                                },
-                                err: function(err) {
-                                    console.error(err);
-                                }
-                            }
-                        });
-                    } else {
-                        results.push(file);
-                        if (!--pending) done.success(results);
-                    }
+            ignoredFiles = opts.filter || function() { return true; },
+            filesToProcess = Bacon
+                .fromNodeCallback(fs.readdir, dir)
+                .map(function(files) {
+                    return files.filter(ignoredFiles);
+                }),
+            filesAndFolderStatus = filesToProcess.flatMapLatest(function(files) {
+                var fileAndStatusStreams = _.map(files, function(file) {
+                    return Bacon.combineTemplate({
+                        file: file,
+                        isFolder: Bacon
+                            .fromNodeCallback(fs.stat, dir + path.sep + file)
+                            .map(function(stat) {
+                                return stat.isDirectory();
+                            })
+                    }).toEventStream();
                 });
-            });
-        });
+                return Bacon.when(fileAndStatusStreams, function() {
+                    return _.map(arguments, function(status) {
+                        return status;
+                    });
+                });
+            }),
+            filesToAdd = filesAndFolderStatus
+                .map(function(files) {
+                    var filtered = _.filter(files, function(file) {
+                        return !file.isFolder;
+                    });
+                    return _.map(filtered, function(file) {
+                        return basedir + path.sep + file.file;
+                    });
+                }),
+            subFoldersToProcessWithFolderStatus = filesAndFolderStatus
+                .map(function(files) {
+                    var filtered = _.filter(files, function(file) {
+                        return file.isFolder;
+                    });
+                    return _.map(filtered, function(file) {
+                        return file.file;
+                    });
+                }),
+            processedSubFolders = subFoldersToProcessWithFolderStatus.map(function(files) {
+                return _.map(files, function(file) {
+                    return walk({
+                        dir: file,
+                        basedir: basedir
+                    });
+                });
+            }),
+            subFolders = processedSubFolders.flatMapLatest(function(subFolderStreams) {
+                if(_.isEmpty(subFolderStreams)) return Bacon.once([]);
+                return Bacon.when(subFolderStreams, function() {
+                    return _.reduce(arguments, function(allSubFolders, subFolder) {
+                        return allSubFolders.concat(subFolder);
+                    }, []);
+                });
+            }),
+            allFiles = Bacon
+                .combineTemplate({
+                    subFolders: subFolders,
+                    filesToAdd: filesToAdd
+                })
+                .map(function(combined) {
+                    return combined.subFolders.concat(combined.filesToAdd);
+                })
+                .take(1);
+        return allFiles;
     };
 
 module.exports = function build() {
@@ -75,15 +91,21 @@ module.exports = function build() {
             .slice(-2)
             .map(function(path) {
                 return path.replace('specs/', '');
-            });
-    ignoredForSpecs.push('main.js');
+            }).concat(['main.js']),
+        specsOpts  = {
+            basedir: './specs',
+            noParse: [
+                './specs/vendor/mocha.js'
+            ],
+            debug: true
+        },
+        allSpecFiles;
     index
         .bundle({'standalone': 'funiframe'})
         .pipe(fs.createWriteStream('./index.js'));
-    walk({
+    allSpecFiles = walk({
         dir:'./specs',
         basedir: '.',
-        done: specsWalkDone,
         filter: function(path) {
             return ignoredForSpecs
                 .filter(function(p) {
@@ -92,6 +114,17 @@ module.exports = function build() {
                 path.indexOf('.js') > -1 &&
                 path.indexOf('vendor') === -1;
         }
+    });
+    allSpecFiles.onValue(function(files) {
+        var allSpecsFileName = createSpecRequireFile('./specs/all.specs.js', files),
+            specs = browserify(specsOpts);
+        specs
+            .add('./main.js')
+            .bundle()
+            .pipe(fs.createWriteStream('./specs/built.js'));
+    });
+    allSpecFiles.onError(function(error) {
+        console.error(error);
     });
     return ['index.js', 'specs/all.specs.js', 'specs/built.js'];
 };
