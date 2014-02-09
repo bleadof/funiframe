68caa5885d0ff827d9d65ef26211c236d9c2bbd3
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2014-02-10 01:00:08 +0200

    Add chai-jquery

--- specs/funiframe.js
+++ specs/funiframe.js
@@ -1,6 +1,12 @@
 var $ = require('jquery'),
-    expect = require('chai').expect,
+    chai = require('chai'),
+    chaiJquery = require('chai-jquery'),
+    expect = chai.expect,
     funiframe = require('../index.js');
+
+chai.use(function(chai, utils) {
+    chaiJquery(chai, utils, $);
+});
 
 describe('FunIFrame', function() {
     afterEach(function() {
@@ -13,16 +19,16 @@ describe('FunIFrame', function() {
                     url: 'http://localhost:8080',
                     to: $container.get(0)
                 });
-            expect($container.find('iframe')).to.have.length(1);
+            expect($container).to.have('iframe');
         });
         it('returns an object with close that removes added iframe from container', function() {
-            var container = $('#funiframe'),
+            var $container = $('#funiframe'),
                 iframe = funiframe.open({
                     url: 'http://localhost:8080',
-                    to: container.get(0)
+                    to: $container.get(0)
                 });
             iframe.close();
-            expect(container.find('iframe')).to.have.length(0);
+            expect($container).to.not.have('iframe');
         });
     });
 });
